41e686f6a87e6e76b40f48635405636cfd87995b
LittleAngel4ever <Minsk_x250@tutamail.com>
2024-11-22 13:27:13 +0300

    finish

---
+++ lion.py
@@ -0,0 +1,7 @@
+import docx
+import matplotlib.pyplot as plt
+from collections import counter
+
+doc = docx.Document('lion.docx')
+text = []
+symbols = ['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я']
